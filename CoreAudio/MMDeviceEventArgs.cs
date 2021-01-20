4c68d889596832d662ecd0cbfaa4e41a467f29e8
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2021-01-20 10:19:09 +0100

    add EventManagerClasses

---
+++ CoreAudio/MMDeviceEventArgs.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CoreAudio
+{
+    public class MMDeviceEventArgs : EventArgs
+    {
+        private readonly MMDevice _device;
+        public MMDeviceEventArgs(MMDevice device)
+        {
+            _device = device;
+        }
+
+        public MMDevice Device
+        {
+            get { return _device; }
+        }
+    }
+    public class MMDeviceRemovedEventArgs
+    {
+        private readonly string _deviceId;
+
+        public MMDeviceRemovedEventArgs(string deviceId)
+        {
+            _deviceId = deviceId;
+        }
+
+        public string DeviceId
+        {
+            get { return _deviceId; }
+        }
+    }
+    public class DefaultAudioDeviceEventArgs : MMDeviceEventArgs
+    {
+        private readonly EDataFlow _flow;
+        private readonly ERole _role;
+        public DefaultAudioDeviceEventArgs(MMDevice device, EDataFlow flow, ERole role) : base(device)
+        {
+            _flow = flow;
+            _role = role;
+        }
+
+        public EDataFlow Flow
+        {
+            get { return _flow; }
+        }
+
+        public ERole Role
+        {
+            get { return _role; }
+        }
+    }
+    public class MMDeviceStateEventArgs : MMDeviceEventArgs
+    {
+        private readonly DEVICE_STATE _newState;
+
+        public MMDeviceStateEventArgs(MMDevice device, DEVICE_STATE newState)
+            : base(device)
+        {
+            _newState = newState;
+        }
+
+        public DEVICE_STATE NewState
+        {
+            get { return _newState; }
+        }
+    }
+}
---
+++ CoreAudio/MMDeviceEventManager.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using CoreAudio.Interfaces;
+
+namespace CoreAudio
+{
+    public class MMDeviceEventManager : IMMNotificationClient, IDisposable
+    {
+        internal static class HResult
+        {
+            public static readonly int OK = 0;
+            public static readonly int NotFound = unchecked((int)0x80070490);
+            public static readonly int FileNotFound = unchecked((int)0x80070002);
+        }
+
+        private readonly IMMDeviceEnumerator _deviceEnumerator = (IMMDeviceEnumerator)new _MMDeviceEnumerator();
+
+        private readonly SynchronizationContext _synchronizationContext;
+
+        public MMDeviceEventManager()
+        {
+            _synchronizationContext = SynchronizationContext.Current;
+
+            int hr = _deviceEnumerator.RegisterEndpointNotificationCallback(this);
+            if (hr != HResult.OK)
+                throw Marshal.GetExceptionForHR(hr);
+        }
+
+        public event EventHandler<MMDeviceEventArgs> DeviceAdded;
+        public event EventHandler<MMDeviceRemovedEventArgs> DeviceRemoved;
+        public event EventHandler<MMDeviceEventArgs> DevicePropertyChanged;
+        public event EventHandler<MMDeviceEventArgs> DefaultDeviceChanged;
+        public event EventHandler<MMDeviceStateEventArgs> DeviceStateChanged;
+
+
+        public void Dispose()
+        {
+            int hr = _deviceEnumerator.UnregisterEndpointNotificationCallback(this);
+            if (hr != HResult.OK)
+                throw Marshal.GetExceptionForHR(hr);
+        }
+
+        public MMDevice GetDevice(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            IMMDevice underlyingDevice;
+            int hr = _deviceEnumerator.GetDevice(id, out underlyingDevice);
+            if (hr == HResult.OK)
+                return new MMDevice(underlyingDevice);
+
+            if (hr == HResult.NotFound)
+                return null;
+
+            throw Marshal.GetExceptionForHR(hr);
+        }
+
+        public void OnDefaultDeviceChanged(EDataFlow flow, ERole role, [In, MarshalAs(UnmanagedType.LPWStr)] string pwstrDeviceId)
+        {
+            InvokeOnSynchronizationContext(() =>
+            {
+                var handler = DefaultDeviceChanged;
+                if (handler != null)
+                {
+                    MMDevice device = null;
+                    if (pwstrDeviceId != null)
+                        device = GetDevice(pwstrDeviceId);
+
+                    handler(this, new DefaultAudioDeviceEventArgs(device, flow, role));
+                }
+            });
+        }
+
+        public void OnDeviceAdded([In, MarshalAs(UnmanagedType.LPWStr)] string pwstrDeviceId)
+        {
+            InvokeOnSynchronizationContext(() =>
+            {
+                var handler = DeviceAdded;
+                if (handler != null)
+                {
+                    MMDevice device = GetDevice(pwstrDeviceId);
+                    if (device == null)
+                        return;     // Device was already removed by the time we got here
+
+                    handler(this, new MMDeviceEventArgs(device));
+                }
+            });
+        }
+
+        public void OnDeviceRemoved([In, MarshalAs(UnmanagedType.LPWStr)] string pwstrDeviceId)
+        {
+            InvokeOnSynchronizationContext(() =>
+            {
+                var handler = DeviceRemoved;
+                if (handler != null)
+                {
+                    handler(this, new AudioDeviceRemovedEventArgs(pwstrDeviceId));
+                }
+            });
+        }
+
+        public void OnDeviceStateChanged([In, MarshalAs(UnmanagedType.LPWStr)] string pwstrDeviceId, DEVICE_STATE dwNewState)
+        {
+            InvokeOnSynchronizationContext(() =>
+            {
+                var handler = DeviceStateChanged;
+                if (handler != null)
+                {
+                    MMDevice device = GetDevice(pwstrDeviceId);
+                    if (device == null)
+                        return;     // Device was already removed by the time we got here
+
+                    handler(this, new AudioDeviceStateEventArgs(device, dwNewState));
+                }
+            });
+        }
+
+        public void OnPropertyValueChanged([In, MarshalAs(UnmanagedType.LPWStr)] string pwstrDeviceId, PROPERTYKEY key)
+        {
+            InvokeOnSynchronizationContext(() =>
+            {
+                var handler = DevicePropertyChanged;
+                if (handler != null)
+                {
+                    MMDevice device = GetDevice(pwstrDeviceId);
+                    if (device == null)
+                        return;     // Device was already removed by the time I got here
+
+                    handler(this, new MMDeviceEventArgs(device));
+                }
+            });
+        }
+
+        private void InvokeOnSynchronizationContext(Action action)
+        {
+            if (_synchronizationContext == null)
+            {
+                action();
+            }
+            else
+            {
+                _synchronizationContext.Post(state => { action(); }, null);
+            }
+        }
+
+
+    }
+}
